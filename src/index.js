343ade0b32249265744522df7a48c2444d5f7dcf
Mycents <prakashsubramani@hotmail.com>
2019-11-14 14:55:30 -0500

    Update apollo-react and setup first query

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 /* import libaries */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ApolloProvider} from 'react-apollo';
 import {ApolloClient} from 'apollo-client';
 import {HttpLink} from 'apollo-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
+import App from './App/index';
 
 /* define variables */
 const GITHUB_BASE_URL = 'https://api.github.com/graphql';
@@ -24,3 +26,11 @@ const client = new ApolloClient({
   link: httpLink,
   cache,
 })
+
+/* render the component */
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
