48f5ad430ed6dc7e90292871be0b7702a54adc4f
Mycents <prakashsubramani@hotmail.com>
2019-11-18 15:40:45 -0500

    Added repository, repositorylist and repository item components. Also added basic styling

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,34 +1,54 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import {Query} from 'react-apollo';
+import RepositoryList from '../Repository';
+import Loading from '../Loading';
 
-const GET_CURRENT_USER = gql`
+const GET_REPOSITORIES_OF_CURRENT_USER = gql`
     {
         viewer {
-            login
-            name
+            repositories(
+                first: 5
+                orderBy: {direction: DESC, field: STARGAZERS}
+            ) {
+                edges {
+                    node {
+                        id
+                        name
+                        url
+                        descriptionHTML
+                        primaryLanguage {
+                            name
+                        }
+                        owner {
+                            login
+                            url
+                        }
+                        stargazers {
+                            totalCount
+                        }
+                        viewerHasStarred
+                        watchers {
+                            totalCount
+                        }
+                        viewerSubscription
+                    }
+                }
+            }
         }
     }
 `;
 
 const Profile = () => (
-    <Query query = {GET_CURRENT_USER}>
+    <Query query = {GET_REPOSITORIES_OF_CURRENT_USER}>
         {({data, loading}) => {
             
             if (loading || !data) {
-                return <div>Loading .....</div>;
+                return <Loading />;
             }
 
-            if (!data) {
-                return null;
-            }
-            
             const {viewer} = data;
-            return (
-                <div>
-                    {viewer.name} {viewer.login}
-                </div>
-            );
+            return <RepositoryList repositories={viewer.repositories} />
         }}
     </Query>
 );
