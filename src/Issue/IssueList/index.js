86a8388227ab6e858a622d497dbe89d8467b5d4c
Mycents <prakashsubramani@hotmail.com>
2019-12-16 14:58:54 -0500

    Implemented client side filter for toggling between open, closed and no issues

--- src/Issue/IssueList/index.js
+++ src/Issue/IssueList/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import IssueItem from '../IssueItem';
 import Loading from '../../Loading';
 import ErrorMessage from '../../Error';
+import { ButtonUnobtrusive } from '../../Button';
+
 import './style.css';
+
+const ISSUE_STATES = { 
+    NONE: 'NONE',
+    OPEN: 'OPEN', 
+    CLOSED: 'CLOSED',
+};
+
+const TRANSITION_LABELS = { 
+    [ISSUE_STATES.NONE]: 'Show Open Issues', 
+    [ISSUE_STATES.OPEN]: 'Show Closed Issues', 
+    [ISSUE_STATES.CLOSED]: 'Hide Issues',
+};
+
+const TRANSITION_STATE = { 
+    [ISSUE_STATES.NONE]: ISSUE_STATES.OPEN, 
+    [ISSUE_STATES.OPEN]: ISSUE_STATES.CLOSED,
+    [ISSUE_STATES.CLOSED]: ISSUE_STATES.NONE,
+};
+
+const isShow = issueState => issueState !== ISSUE_STATES.NONE;
 
 const GET_ISSUES_OF_REPOSITORY = gql`
   query($repositoryOwner: String!, $repositoryName: String!) {
@@ -26,34 +48,67 @@ const GET_ISSUES_OF_REPOSITORY = gql`
 } 
 `;
 
-const Issues = ({ repositoryOwner, repositoryName }) => (
-    <div className="Issues">
-        <Query
-            query={GET_ISSUES_OF_REPOSITORY}
-            variables={{
-                repositoryOwner,
-                repositoryName,
-            }}
-        >
-         {({ data, loading, error }) => {
-            if (error) {
-              return <ErrorMessage error={error} />;
-            }
+class Issues extends Component {
+    state = {
+      issueState: ISSUE_STATES.NONE,
+    };
 
-            if (loading && !data) {
-                return <Loading />;
-            }
+    onChangeIssueState = nextIssueState => {
+        this.setState({ issueState: nextIssueState });
+    };
 
-            const { repository } = data;
-            if (!repository.issues.edges.length) {
-                return <div className="IssueList">No issues ...</div>;
-            }
+    render() {
+      const { issueState } = this.state;
+      const { repositoryOwner, repositoryName } = this.props;
+  
+      return (
+            <div className="Issues">
+                <ButtonUnobtrusive
+                    onClick={() =>
+                    this.onChangeIssueState(TRANSITION_STATE[issueState])
+                }
+            >
+                    {TRANSITION_LABELS[issueState]}
+                </ButtonUnobtrusive>
+
+                {isShow(issueState) && (
+                <Query
+                    query={GET_ISSUES_OF_REPOSITORY}
+                    variables={{
+                        repositoryOwner,
+                        repositoryName,
+                    }}
+                >
+                {({ data, loading, error }) => {
+                    if (error) {
+                    return <ErrorMessage error={error} />;
+                    }
+
+                    if (loading && !data) {
+                        return <Loading />;
+                    }
+
+                    const { repository } = data;
+                    const filteredRepository = {
+                        issues: {
+                          edges: repository.issues.edges.filter(
+                            issue => issue.node.state === issueState,
+                            ), 
+                        },
+                    };
         
-            return <IssueList issues={repository.issues} />;
-         }}
-        </Query>
-    </div>
-);
+                    if (!filteredRepository.issues.edges.length) {
+                        return <div className="IssueList">No issues ...</div>;
+                    }
+                
+                    return <IssueList issues={filteredRepository.issues} />;
+                }}
+                </Query>
+                )}
+            </div>
+        );
+    }
+}
 
 const IssueList = ({ issues }) => (
     <div className="IssueList">
